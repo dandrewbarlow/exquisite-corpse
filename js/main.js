73ac726c7577448a8c6cc6d229b5134a6b21d836
Andrew Barlow <dandrewbarlow@gmail.com>
2020-02-06 16:46:42 -0700

    bones sorta working

--- js/main.js
+++ js/main.js
@@ -1,8 +1,24 @@
 
-// A function to animate bones falling down the page
+
+// boneFall : A function to animate bones falling down the page
+// init
+var pos1 = 0;
+var pos2 = 50;
+
 function boneFall() {
-    var bone1 = document.getElementsById("bone1");
-    var bone2 = document.getElementsById("bone2");
-    
+    var bone1 = document.getElementById("bone1");
+    var bone2 = document.getElementById("bone2");
+
+
+    pos1 += 1;
+
+    pos2 += 1;
+    bone1.style.top = pos1 + "%";
+    bone2.style.top = pos2 + "%";
+
+    // edge detection
+
 
 }
+
+window.setInterval(boneFall, 100);

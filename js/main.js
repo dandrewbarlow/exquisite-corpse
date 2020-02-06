a3d83c425ae3d75fdcc6d99571a15a1502761f6e
Andrew Barlow <dandrewbarlow@gmail.com>
2020-02-06 16:38:10 -0700

    new branch for script

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+// A function to animate bones falling down the page
+function boneFall() {
+    var bone1 = document.getElementsById("bone1");
+    var bone2 = document.getElementsById("bone2");
+    
+
+}

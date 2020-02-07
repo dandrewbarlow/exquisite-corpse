52ac8cfa90f946728bef9a1466be0fd5fdd50438
Andrew Barlow <dandrewbarlow@gmail.com>
2020-02-06 17:10:27 -0700

    boneFall almost done

--- js/main.js
+++ js/main.js
@@ -4,26 +4,29 @@
 // init
 var pos1 = 0;
 var pos2 = 50;
+var ticker = 0;
+
+var height = Math.max( document.body.scrollHeight, document.body.offsetHeight);
 
 function boneFall() {
     var bone1 = document.getElementById("bone1");
     var bone2 = document.getElementById("bone2");
 
 
-    pos1 += 1;
+    pos1 += 5;
 
-    pos2 += 1;
-    bone1.style.top = pos1 + "%";
-    bone2.style.top = pos2 + "%";
+    pos2 += 5;
+    bone1.style.top = pos1 + "px";
+    bone2.style.top = pos2 + "px";
 
     // edge detection
-    if (pos1 > 100) {
+    if (pos1 > height - 40) {
       pos1 = 0;
     }
-    if (pos2 > 100) {
+    if (pos2 > height - 40) {
       pos2 = 0;
     }
 
 }
 
-window.setInterval(boneFall, 100);
+window.setInterval(boneFall, 50);
